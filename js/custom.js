a94c6241141c8d3bb2e3a932897ba56374e70014
Kedar Kulkarni <k.kulkarni@interproductec.com>
2017-04-29 11:42:05 +0530

    added new files

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,16 @@
 /* Custom JS file */
+/*
+$(window).load(
+	function(){
+		"use strict";
+		$(".loader-item").delay(700).fadeOut(),
+		$("#pageloader").delay(1200).fadeOut("slow")
+	}
+)
+*/
+/*
+	$(document).ready(function(){
+	  $('.slides-container').slick();
+	});
+
+*/
